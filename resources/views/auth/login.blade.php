513d4b0f4203a36e97b4ef1a8ce53803363b2ec6
olexin <aizharolexin@yandex.kz>
2024-07-07 15:46:33 +0500

    feat(ui): login page tailwind

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,82 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
+    <div class="auth-page">
+        <div class="header-wrapper">
+            <img class="logo"
+                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                 alt="Your Company">
+            <h2 class="header-title">
+                {{ __('Login') }}
+            </h2>
+        </div>
+
+        <div class="page-container">
+            <form class="space-y-6" action="{{ route('login') }}" method="POST">
+                @csrf
+                <div>
+                    <label for="email" class="">{{ __('Email Address') }}</label>
+                    <div class="mt-2">
+                        <input id="email" name="email" type="email" class="@error('email') is-invalid @enderror"
+                               autocomplete="email" required>
+                    </div>
+
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+
+                <div>
+                    <div class="flex items-center justify-between">
+                        <label for="password" class="">{{ __('Password') }}</label>
+                        @if (Route::has('password.request'))
+                            <div class="text-sm">
+                                <a href="{{ route('password.request') }}">{{ __('Forgot Your Password?') }}</a>
+                            </div>
+                        @endif
+                    </div>
+                    <div class="mt-2">
+                        <input id="password" name="password" type="password"
+                               class="@error('email') is-invalid @enderror" autocomplete="current-password" required>
+                    </div>
+                    @error('password')
+                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                    @enderror
+                </div>
+
+                <div class="flex items-center">
+                    <input class=""
+                           type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                    <label class="form-check-label" for="remember">
+                        {{ __('Remember Me') }}
+                    </label>
+                </div>
+
+                <div>
+                    <button type="submit" class="">{{ __('Login') }}</button>
+                </div>
+            </form>
+
+            <p class="mt-10 text-center text-sm text-gray-500">
+                Not a member?
+                <a href="#">
+                    {{__('Register')}}
+                </a>
+            </p>
+        </div>
+    </div>
+
+    {{--
+<div class="container mx-auto">
+    <div class="flex justify-center items-center w-full h-full">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+            <div class="border rounded-xl p-4">
+                <div class="text-2xl font-bold"></div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -69,5 +140,5 @@
             </div>
         </div>
     </div>
-</div>
+</div>--}}
 @endsection
